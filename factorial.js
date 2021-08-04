7f449ac846a3ec02f86f31f311edfe7bd7d01be1
Shahin-Mahmud98 <shahinmahmud412@gmail.com>
2021-08-04 20:52:28 +0600

    add

---
+++ factorial.js
@@ -0,0 +1,9 @@
+// 3! = 3 x 2 x 1
+
+var factorial = 1;
+for (var i = 1; i <= 7; i++) {
+    // console.log(i);
+    // var factorial = 1;
+    factorial = factorial * i;
+}
+console.log(factorial);
